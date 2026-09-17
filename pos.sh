bf436c024a4c8b3a6c10abf6a06b5abf1323643b
Onix <onixbreak123@gmail.com>
2026-09-17 15:42:57 -0600

    test de mariaDB para android

--- pos.sh
+++ pos.sh
@@ -4,9 +4,27 @@ echo "================================"
 echo "      DISTRIBUIDORA MÉDICA"
 echo "================================"
 echo ""
-echo "iniciando mariaDB..."
-mysqld_safe &
+#Evitamos que android suspenda termux
+termux-wake-lock
 
-sleep 3
+#comprobamos mariaDB
+if pgrep mariadb > /dev/null; then
+    echo "MariaDB ya está activo"
+
+ else
+    echo "iniciando mariaDB..."
+    mysqld_safe &
+    fi
+echo "Esperando MariaDB..."
+for i in {1..10}; do
+    if mariadb-admin ping -u onix -psystem15 --silent 2>/dev/null; then
+    echo "MariaDB Ejecutandose"
+    break;
+
+    fi
+
+    sleep 1;
+    done
+    
 echo "Iniciando NodeJS..."
 node app.js
